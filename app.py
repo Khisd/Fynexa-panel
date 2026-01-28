7b9eee65187cd514cc3d63b553a38ea7bee28f47
TheRecord3r <168276116+Khisd@users.noreply.github.com>
2026-01-29 00:56:00 +0800

    Retrieve GITHUB_TOKEN from environment variables
    Add GITHUB_TOKEN retrieval from environment variables

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 if __name__ == "__main__":
     import os
+    GITHUB_TOKEN = os.environ.get("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")
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
